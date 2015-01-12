6f82b0e77cbb0b1b1cf260e03e9aaf34fc23bb11
Mathieu Blondel <mathieu@mblondel.org>
2015-01-13 00:23:40 +0900

    Fix joblib import.

--- spira/datasets.py
+++ spira/datasets.py
@@ -3,7 +3,11 @@
 import os
 
 from sklearn.datasets.base import get_data_home as _get_data_home
-from sklearn.externals import joblib
+
+try:
+    import joblib
+except ImportError:
+    from sklearn.externals import joblib
 
 
 def get_data_home():
