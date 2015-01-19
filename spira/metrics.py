5f7e07e313ae1efcc636e78ced907be17bdfc063
Mathieu Blondel <mathieu@mblondel.org>
2015-01-19 23:44:24 +0900

    Add precision, recall and f1_score.

--- spira/metrics.py
+++ spira/metrics.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.sparse as sp
 
 
-def rmse(X_true, X_pred):
+def _check(X_true, X_pred):
     if X_true.shape != X_pred.shape:
         raise ValueError("X_true and X_pred should have the same shape.")
 
@@ -11,5 +11,34 @@ def rmse(X_true, X_pred):
     X_true = sp.csr_matrix(X_true)
     X_pred = sp.csr_matrix(X_pred)
 
+    return X_true, X_pred
+
+
+def rmse(X_true, X_pred):
+    X_true, X_pred = _check(X_true, X_pred)
     mse = np.mean((X_true.data - X_pred.data) ** 2)
     return np.sqrt(mse)
+
+
+def precision(X_true, X_pred):
+    X_true, X_pred = _check(X_true, X_pred)
+    tp = np.logical_and(X_true.data == 1, X_pred.data == 1).sum()
+    n_pos_pred = np.sum(X_pred.data == 1)
+    # Proportion of truly positives in predicted positives.
+    return float(tp) / n_pos_pred
+
+
+def recall(X_true, X_pred):
+    X_true, X_pred = _check(X_true, X_pred)
+    tp = np.logical_and(X_true.data == 1, X_pred.data == 1).sum()
+    n_pos = np.sum(X_true.data == 1)
+    # Proportion of positives correctly predicted as positive.
+    return float(tp) / n_pos
+
+
+def f1_score(X_true, X_pred):
+    X_true, X_pred = _check(X_true, X_pred)
+    p = precision(X_true, X_pred)
+    r = recall(X_true, X_pred)
+    # Harmonic mean of precision and recall.
+    return 2 * p * r / (p + r)
