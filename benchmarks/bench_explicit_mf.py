6d5073c42b7e51b763348a14f388743230e4d458
Mathieu Blondel <mathieu@mblondel.org>
2015-01-22 00:40:04 +0900

    Fix and rename benchmark.

--- benchmarks/bench_explicit_mf.py
+++ benchmarks/bench_explicit_mf.py
@@ -3,7 +3,7 @@ import time
 
 from spira.datasets import load_movielens
 from spira.cross_validation import train_test_split
-from spira.completion import MatrixFactorization
+from spira.completion import ExplicitMF
 
 try:
     version = sys.argv[1]
@@ -16,8 +16,8 @@ print X.shape
 X_tr, X_te = train_test_split(X, train_size=0.75, random_state=0)
 
 start = time.time()
-mf = MatrixFactorization(n_components=30, max_iter=10,
-                         alpha=1e-1, random_state=0, verbose=1)
+mf = ExplicitMF(n_components=30, max_iter=10, alpha=1e-1, random_state=0,
+                verbose=1)
 mf.fit(X_tr)
 print "Time", time.time() - start
 print "RMSE", mf.score(X_te)
